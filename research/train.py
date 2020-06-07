97ad3bc71bb2144455cf1d7cac09293a9660bc75
goodmattg <mattgoodman13@gmail.com>
2020-06-07 18:36:23 -0500

    smoothed out ignite metrics vs. logging issues. Helper function to remove lambdas from user

--- research/train.py
+++ research/train.py
@@ -8,7 +8,12 @@ from ignite.metrics import Accuracy, Loss
 from dataset import get_dataloaders
 from networks import get_network
 
-from utils.engine_logging import log_training_loss, log_training_results
+from utils.logging import (
+    _lf,
+    log_training_loss,
+    log_training_results,
+    log_fields_stdout,
+)
 
 
 def create_supervised_trainer(cfg: DictConfig, device="cpu") -> Engine:
@@ -22,12 +27,10 @@ def create_supervised_trainer(cfg: DictConfig, device="cpu") -> Engine:
     # Loss
     loss_fn = torch.nn.NLLLoss()
 
-    metrics = {"accuracy": Accuracy(), "nll": Loss(loss_fn)}
-
     def _update(engine, batch):
 
         ########################################################################
-        # Modify the logic of your training loop
+        # Modify the logic of your training
         ########################################################################
         model.train()
         optimizer.zero_grad()
@@ -37,15 +40,25 @@ def create_supervised_trainer(cfg: DictConfig, device="cpu") -> Engine:
         loss = loss_fn(y_pred, y)
         loss.backward()
         optimizer.step()
-        return loss.item(), y_pred, y
 
-    def _metrics_transform(output):
-        return output[1], output[2]
+        # Anything you want to log must be returned in this dictionary
+        update_dict = {"nll": loss.item(), "y_pred": y_pred, "y": y}
+        return update_dict
 
     engine = Engine(_update)
 
+    # Common metrics require base "y_pred" and "y". Lambda to reduce verbosity in metrics
+    _ypred_y = lambda _: (_["y_pred"], _["y"])
+
+    # Specify metrics. "output_transform" used to select items from "update_dict" needed by metrics
+    # https://pytorch.org/ignite/metrics.html#ignite.metrics.Loss
+    metrics = {
+        "accuracy": Accuracy(output_transform=_ypred_y),
+        "nll": Loss(loss_fn, output_transform=_ypred_y),
+    }
+
+    # Attach metrics to engine
     for name, metric in metrics.items():
-        metric._output_transform = _metrics_transform
         metric.attach(engine, name)
 
     return engine
@@ -69,7 +82,12 @@ def train(cfg: DictConfig) -> None:
     # Callbacks
     trainer.add_event_handler(Events.STARTED, lambda engine: print("Start training"))
     # trainer.add_event_handler(Events.EPOCH_COMPLETED, log_training_results)
-    trainer.add_event_handler(Events.ITERATION_COMPLETED, log_training_loss)
+    # trainer.add_event_handler(
+    #     Events.ITERATION_COMPLETED, lambda _: log_training_loss(_, field="nll")
+    # )
+    trainer.add_event_handler(
+        Events.ITERATION_COMPLETED, _lf(log_fields_stdout, ["nll", "nll"])
+    )
 
     # train()
     trainer.run(train_loader, max_epochs=cfg.mode.train.max_epochs)
--- research/utils/engine_logging.py
+++ research/utils/engine_logging.py
@@ -1,9 +1,25 @@
+from typing import Callable, List
 from ignite.engine import Engine
 
 
+def _lf(log_fn: Callable[[Engine, List[str]], None], fields: List[str]):
+    """Returns a log function lambda. Useful for code clarity"""
+    return lambda _: log_fn(_, fields=fields)
+
+
+def log_fields_stdout(engine: Engine, fields: List[str]) -> None:
+    """Log several fields in the engine output dictionary to stdout"""
+    log_str = "  ".join(
+        ["{}: {:.2f}".format(field, engine.state.output[field]) for field in fields]
+    )
+    print("Epoch[{}] {}".format(engine.state.epoch, log_str))
+
+
 # FIXME: Use logging module, not print statements
-def log_training_loss(engine: Engine) -> None:
-    print("Epoch[{}] Loss: {:.2f}".format(engine.state.epoch, engine.state.output[0]))
+def log_training_loss(engine: Engine, field: str = "loss") -> None:
+    print(
+        "Epoch[{}] Loss: {:.2f}".format(engine.state.epoch, engine.state.output[field])
+    )
 
 
 # FIXME: Use logging module, not print statements
