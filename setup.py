8637de5c7deb4918c918cd90c02b21f9fcb088ec
goodmattg <mattgoodman13@gmail.com>
2020-06-18 15:07:57 -0500

    required dependencies now up to date

--- setup.py
+++ setup.py
@@ -18,10 +18,13 @@ PACKAGES = ["research"]
 # What packages are required for this module to be executed?
 REQUIRED = [
     "hydra-core>=1.0.0-rc1",
-    "omegaconf>=1.4.1",
+    "omegaconf>=2.0.1rc6",
     "opencv-python>=4.2.0",
     "matplotlib>=3.2.1",
     "visdom>=0.1.8.9",
+    "torch>=1.5.0",
+    "pytorch-ignite>=0.3.0",
+    "torchvision>=0.6.0",
 ]
 
 # What packages are optional?
